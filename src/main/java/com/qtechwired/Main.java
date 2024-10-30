62aba1f53ffc8c4237ee056f73471ae3c1f3c4f5
DieroKreator <qtechwired@gmail.com>
2024-10-30 17:26:56 -0300

    first commit

---
+++ src/main/java/com/qtechwired/Main.java
@@ -0,0 +1,14 @@
+package com.qtechwired;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static float calcularAreaCuadrado(float num1, float num2) {
+        float resultado = num1 * num2;
+        System.out.println("Area do cuadrado: " + resultado);  
+        return resultado;
+    }
+
+}
